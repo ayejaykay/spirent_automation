094b11aaf0074c6997be1f6b448ae942ba72c196
Anthony Kahley <akahley2018@gmail.com>
2024-01-17 12:27:08 -0500

    Test progress bar

--- spirent_automation-master/Tcl/lib/teapot/package/win32-ix86/lib/Memchan2.3/pkgIndex.tcl
+++
@@ -1,2 +0,0 @@
-package ifneeded Memchan 2.3 \
-    [list load [file join $dir Memchan23.dll]]
--- spirent_automation-master/Tcl/lib/teapot/package/win32-ix86/lib/tls1.6.4/pkgIndex.tcl
+++
@@ -1,3 +0,0 @@
-package ifneeded tls 1.6.4 \
-    "[list source [file join $dir tls.tcl]] ; \
-     [list tls::initlib $dir tls164.dll]"
